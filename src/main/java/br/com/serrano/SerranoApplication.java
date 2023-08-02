7074de28c022e0beae4d2575d582a2fe4f9e43a1
Kaio Gabrioti <kgabrioti@gmail.com>
2023-08-02 11:51:57 -0300

    criando projeto backend serrano

---
+++ src/main/java/br/com/serrano/SerranoApplication.java
@@ -0,0 +1,13 @@
+package br.com.serrano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerranoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerranoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/serrano/SerranoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.serrano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerranoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
